33b8a55194956c4f71857648440a92e9d3cfcb79
sathvika27 <sathvika.gambeer@gmail.com>
2020-03-20 17:29:34 +0530

    Added autoIOC

---
+++ autoIOC/iocBoot/iocautoIOC/st.cmd
@@ -0,0 +1,26 @@
+#!../../bin/linux-x86_64/autoIOC
+
+## You may have to change autoIOC to something else
+## everywhere it appears in this file
+
+< envPaths
+
+cd "${TOP}"
+
+## Register all support components
+dbLoadDatabase "dbd/autoIOC.dbd"
+autoIOC_registerRecordDeviceDriver pdbbase
+
+## Load record instances
+dbLoadRecords("db/autoDb.db")
+
+cd "${TOP}/iocBoot/${IOC}"
+set_requestfile_path("$(TOP)/autoSaveRestore")
+set_savefile_path("$(TOP)/autoSaveRestore")
+set_pass0_restoreFile("auto.sav")
+set_pass1_restoreFile("auto.sav")
+iocInit
+create_monitor_set("auto.req", 5)
+
+## Start any sequence programs
+#seq sncxxx,"user=epics"
